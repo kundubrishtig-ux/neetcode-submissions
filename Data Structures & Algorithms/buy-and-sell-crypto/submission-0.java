9d2f95d3882000120a326e209bb6959ec3e880f6
Brishti Kundu <kundu.brishtig@gmail.com>
2026-08-01 10:18:58 +0530

    Add: buy-and-sell-crypto - submission-0

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-0.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+int maxprofit=0;
+int minprice=Integer.MAX_VALUE;
+for(int price:prices)
+{
+    if(price<minprice)
+    {
+        minprice=price;
+    }
+    else
+    {
+        maxprofit=Math.max(maxprofit,price-minprice);
+    }
+}
+return maxprofit;
+
+    }
+}
